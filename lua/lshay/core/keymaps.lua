f91ad081e994bb0dec43fc7e971f0c75c4f8ee80
Levy, Shay <shay.levy@intel.com>
2023-01-02 21:14:49 +0200

    slight changes

--- lua/lshay/core/keymaps.lua
+++ lua/lshay/core/keymaps.lua
@@ -8,6 +8,7 @@ keymap.set("i", "jk", "<ESC>")
 
 -- clear search highlight
 keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>")
 
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x')
@@ -20,6 +21,17 @@ keymap.set("n", "<leader>-", "<C-x>") -- decrement
 keymap.set("n", "[e", ":move -2<CR>")
 keymap.set("n", "]e", ":move +1<CR>")
 
+-- center the buffer screen for search
+keymap.set("n", "n", "nzz")
+keymap.set("n", "N", "Nzz")
+
+-- Reselect text after visual indent
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+-- move selected line / block of text in visual mode
+keymap.set("x", "K", ":move '<-2<CR>gv-gv")
+keymap.set("x", "J", ":move '<+1<CR>gv-gv")
 
 ----------------------
 -- Plugin Keybinds
@@ -37,26 +49,24 @@ keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current c
 -- restart lsp server (not on youtube nvim video)
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
 
-
 -- Key Map groups using which key
 -- import which-key plugin safely
 local status, wk = pcall(require, "which-key")
 if not status then
-  return
+	return
 end
-
 
 -- Telescope Find Option
 wk.register({
-  f = {
-    name = "find", -- optional group name
-    f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
-    s = { "<cmd>Telescope live_grep<cr>", "Find in Working Dir" },
-    c = { "<cmd>Telescope grep_string<cr>", "Find cur_string in Working Dir" },
-    b = { "<cmd>Telescope buffers<cr>", "List open buffers" },
-    h = { "<cmd>Telescope help_tags<cr>", "List available help tags" },
-    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File"}, --, noremap=false, buffer = 123 }, -- additional options for creating the keymap
-  },
+	f = {
+		name = "find", -- optional group name
+		f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
+		s = { "<cmd>Telescope live_grep<cr>", "Find in Working Dir" },
+		c = { "<cmd>Telescope grep_string<cr>", "Find cur_string in Working Dir" },
+		b = { "<cmd>Telescope buffers<cr>", "List open buffers" },
+		h = { "<cmd>Telescope help_tags<cr>", "List available help tags" },
+		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" }, --, noremap=false, buffer = 123 }, -- additional options for creating the keymap
+	},
 }, { prefix = "<leader>" })
 
 -- vimspector debug keymaps
@@ -66,64 +76,57 @@ keymap.set("n", "<S-l>", ":call vimspector#StepInto()<cr>")
 keymap.set("n", "<S-j>", ":call vimspector#StepOver()<cr>")
 
 wk.register({
-  d = {
-    name = "debug",
-    a = { "<cmd>call vimspector#Continue()<cr>", "Continue"},
-    _ = { ":call vimspector#Reset()<cr>", "Reset"},
-    s = { ":call vimspector#Restart()<cr>", "Restart"},
-    n = { ":call vimspector#Continue()<cr>", "Continue"},
-    rc = { ":call vimspector#RunToCursor()<cr>", "Run to Cursor"},
-    h = { ":call vimspector#ToggleBreakpoint()<cr>", "Toggle Breakpoint"},
-    e = { ":call vimspector#ToggleConditionalBreakpoint()<cr>", "Toggle Conditional Breakpoint"},
-    x = { ":call vimspector#ClearBreakpoints()<cr>", "Clear Breakpoints"},
-    K = { ":call vimspector#StepOut()<cr>", "Step Out"},
-    L = { ":call vimspector#StepInto()<cr>", "Step Into"},
-    J = { ":call vimspector#StepOver()<cr>", "Step Over"},   },
-}, { prefix = "<leader>"})
+	d = {
+		name = "debug",
+		a = { "<cmd>call vimspector#Continue()<cr>", "Continue" },
+		_ = { ":call vimspector#Reset()<cr>", "Reset" },
+		s = { ":call vimspector#Restart()<cr>", "Restart" },
+		n = { ":call vimspector#Continue()<cr>", "Continue" },
+		rc = { ":call vimspector#RunToCursor()<cr>", "Run to Cursor" },
+		h = { ":call vimspector#ToggleBreakpoint()<cr>", "Toggle Breakpoint" },
+		e = { ":call vimspector#ToggleConditionalBreakpoint()<cr>", "Toggle Conditional Breakpoint" },
+		x = { ":call vimspector#ClearBreakpoints()<cr>", "Clear Breakpoints" },
+		K = { ":call vimspector#StepOut()<cr>", "Step Out" },
+		L = { ":call vimspector#StepInto()<cr>", "Step Into" },
+		J = { ":call vimspector#StepOver()<cr>", "Step Over" },
+	},
+}, { prefix = "<leader>" })
 
 -- window management
 
 wk.register({
-    s = {
-        name = "Split Window",
-        v = { "<C-w>v", "Split Vertically"}, -- split window vertically
-        h = { "<C-w>s", "Split Horizontally"}, -- split window horizontally
-        e = { "<C-w>=", "Equal Split"}, -- make split windows equal width & height
-        x = { ":close<CR>", "Close Split"}, -- close current split window
-        m = { ":MaximizerToggle<CR>", "Maximize"}, -- toggle split window maximization
-
-    },
-},
-    { prefix = "<leader>"}
-)
+	s = {
+		name = "Split Window",
+		v = { "<C-w>v", "Split Vertically" }, -- split window vertically
+		h = { "<C-w>s", "Split Horizontally" }, -- split window horizontally
+		e = { "<C-w>=", "Equal Split" }, -- make split windows equal width & height
+		x = { ":close<CR>", "Close Split" }, -- close current split window
+		m = { ":MaximizerToggle<CR>", "Maximize" }, -- toggle split window maximization
+	},
+}, { prefix = "<leader>" })
 
 wk.register({
-    t = {
-        name = "Tabs",
-        o = { ":tabnew<CR>", "Open New"}, -- open new tab
-        x = { ":tabclose<CR>", "Close"}, -- close current tab
-        n = { ":tabn<CR>", "Next"}, --  go to next tab
-        p = { ":tabp<CR>", "Previous"}, --  go to previous tab
-    },
-},
-    { prefix = "<leader>"}
-)
+	t = {
+		name = "Tabs",
+		o = { ":tabnew<CR>", "Open New" }, -- open new tab
+		x = { ":tabclose<CR>", "Close" }, -- close current tab
+		n = { ":tabn<CR>", "Next" }, --  go to next tab
+		p = { ":tabp<CR>", "Previous" }, --  go to previous tab
+	},
+}, { prefix = "<leader>" })
 
 -- using LineBuffer plugin
 wk.register({
-    b = {
-        name = "Buffers",
-        l = { ":BufferLineCycleNext<CR>", "Next"}, -- next buffer
-        h = { ":BufferLineCyclePrev<CR>", "Previous"}, -- prev buffer
-        p = { ":BufferLineTogglePin<CR>", "Pin"}, -- pin buffer
-    },
-},
-    { prefix = "<leader>"}
-)
+	b = {
+		name = "Buffers",
+		l = { ":BufferLineCycleNext<CR>", "Next" }, -- next buffer
+		h = { ":BufferLineCyclePrev<CR>", "Previous" }, -- prev buffer
+		p = { ":BufferLineTogglePin<CR>", "Pin" }, -- pin buffer
+	},
+}, { prefix = "<leader>" })
 
 for i = 1, 9 do
-  keymap.set("n", "<leader>b" .. i, function()
-    require("bufferline").go_to_buffer(i, true)
-  end)
+	keymap.set("n", "<leader>b" .. i, function()
+		require("bufferline").go_to_buffer(i, true)
+	end)
 end
-
--- lua/lshay/plugins-setup.lua
+++ lua/lshay/plugins-setup.lua
@@ -1,13 +1,13 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
@@ -23,119 +23,120 @@ vim.cmd([[
 -- import packer safely
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+	return
 end
 
 -- add list of plugins to install
 return packer.startup(function(use)
-  -- packer can manage itself
-  use("wbthomason/packer.nvim")
+	-- packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
-  
-  -- color schemes
-  use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
-  use("markvincze/panda-vim")
-  use("sainnhe/everforest")
-  use { "ellisonleao/gruvbox.nvim" }
-  
-  -- split window support
-  use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
-  use("szw/vim-maximizer") -- maximizes and restores current window
-  
-  -- essential plugins
-  use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
-  use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
-  
-  -- commenting with gc
-  use("numToStr/Comment.nvim")
+	use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
 
-  -- file explorer
-  use("nvim-tree/nvim-tree.lua")
+	-- color schemes
+	use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
+	use("markvincze/panda-vim")
+	use("sainnhe/everforest")
+	use({ "ellisonleao/gruvbox.nvim" })
 
-  -- vs-code like icons
-  use("nvim-tree/nvim-web-devicons")
+	-- split window support
+	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+	use("szw/vim-maximizer") -- maximizes and restores current window
 
-  -- statusline
-  use("nvim-lualine/lualine.nvim")
+	-- essential plugins
+	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
+	use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
 
-  -- fuzzy finding w/ telescope
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x",
-    require = {{'nvim-lua/plenary.nvim'} } } ) -- fuzzy finder
+	-- commenting with gc
+	use("numToStr/Comment.nvim")
 
-  -- autocompletion
-  use("hrsh7th/nvim-cmp") -- completion plugin
-  use("hrsh7th/cmp-buffer") -- source for text in buffer
-  use("hrsh7th/cmp-path") -- source for file system paths
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
 
-  -- snippets
-  use("L3MON4D3/LuaSnip") -- snippet engine
-  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-  use("rafamadriz/friendly-snippets") -- useful snippets
+	-- vs-code like icons
+	use("nvim-tree/nvim-web-devicons")
 
-  -- managing & installing lsp servers, linters & formatters
-  use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-  use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+	-- statusline
+	use("nvim-lualine/lualine.nvim")
 
-  -- configuring lsp servers
-  use("neovim/nvim-lspconfig") -- easily configure language servers
+	-- fuzzy finding w/ telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", require = { { "nvim-lua/plenary.nvim" } } }) -- fuzzy finder
 
-  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-  use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
-  -- use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	-- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
 
-  -- debugger
-  use("puremourning/vimspector")
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
 
-  -- formatting & linting
-  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-  use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	-- managing & installing lsp servers, linters & formatters
+	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
 
-  -- treesitter configuration
-  use({
-      "nvim-treesitter/nvim-treesitter",
-      run = function()
-          local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-          ts_update()
-      end,
-  })
+	-- configuring lsp servers
+	use("neovim/nvim-lspconfig") -- easily configure language servers
 
-  -- Nvim Neo Tree - alternative to nvim-tree
-  use({
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v2.x",
-      requires = {
-          "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-          "MunifTanjim/nui.nvim",
-      },
-  })
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+	-- use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
-  -- auto closing
-  use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+	-- debugger
+	use("puremourning/vimspector")
 
-  -- git integration
-  use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
-  -- better tab support
-  -- use({
-  --     "romgrk/barbar.nvim",
-  --     requires = {
-  --         "nvim-tree/nvim-web-devicons",
-  --     },
-  -- })
+	-- treesitter configuration
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 
-  -- tabs
-  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+	-- Nvim Neo Tree - alternative to nvim-tree
+	use({
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	})
 
-  -- which-key
-  use {'folke/which-key.nvim'}
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
-  if packer_bootstrap then
-      require("packer").sync()
-  end
+	-- git integration
+	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+	-- better tab support
+	-- use({
+	--     "romgrk/barbar.nvim",
+	--     requires = {
+	--         "nvim-tree/nvim-web-devicons",
+	--     },
+	-- })
+
+	-- tabs
+	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
+
+	-- which-key
+	use({ "folke/which-key.nvim" })
+
+	-- rainbow parentheses
+	use({ "p00f/nvim-ts-rainbow" })
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
