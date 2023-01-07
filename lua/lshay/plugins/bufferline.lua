172b33ebb9a0340eef60d2181036f25cc8f32171
Levy, Shay <shay.levy@intel.com>
2023-01-07 14:00:57 +0200

    Added UI Dresser. Tree view has a tab and M is git modified. Telescope ctrl-q will do smart send to qflist

--- lua/lshay/plugins/bufferline.lua
+++ lua/lshay/plugins/bufferline.lua
@@ -1,21 +1,21 @@
 local setup, bufferline = pcall(require, "bufferline")
 if not setup then
-    return
+	return
 end
 
 bufferline.setup({
-    options = {
-        separator_style = 'slant',
-        hover = {
-            enabled = true,
-            delay = 200,
-            reveal = {'close'}
-        },
-        numbers = "ordinal",
-        indicator = {
-            style = 'underline',
-        },
-        diagnostics = "nvim_lsp",
-    },
+	options = {
+		separator_style = "slant",
+		hover = {
+			enabled = true,
+			delay = 200,
+			reveal = { "close" },
+		},
+		numbers = "ordinal",
+		indicator = {
+			style = "underline",
+		},
+		diagnostics = "nvim_lsp",
+		offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "center" } },
+	},
 })
-
